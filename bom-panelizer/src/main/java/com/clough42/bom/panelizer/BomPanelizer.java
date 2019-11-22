1f11c64872b49dec807224e9ba9b9334034c4e20
clough <james.clough@hp.com>
2019-11-21 21:13:49 -0700

    Initial scaffolding

---
+++ bom-panelizer/src/main/java/com/clough42/bom/panelizer/BomPanelizer.java
@@ -0,0 +1,13 @@
+package com.clough42.bom.panelizer;
+
+/**
+ * Hello world!
+ *
+ */
+public class BomPanelizer 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ bom-panelizer/src/test/java/com/clough42/bom/panelizer/BomPanelizerTest.java
@@ -0,0 +1,25 @@
+package com.clough42.bom.panelizer;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple BomPanelizer.
+ */
+public class BomPanelizerTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    
+    @Test
+    public void executeMain() {
+      BomPanelizer.main(null);
+    }
+}
